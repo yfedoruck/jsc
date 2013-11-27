2d84a66b6116b90e2f4a38acfbe07d4e88f0f71d
yfedoruck <yfedoruck@gmail.com>
2013-11-27 11:24:26 +0200

    --

--- 03/task3.js
+++ 03/task3.js
@@ -1,8 +1,9 @@
 var Person = function(args){
-        var obj = {};
+        var obj = {},
+            pubObj = {};
 
         if (typeof args !== "object"){
-            return obj;
+            return pubObj;
         }
         var capLetter = function(str) {
             return str.charAt(0).toUpperCase() + str.slice(1);
@@ -19,34 +20,24 @@ var Person = function(args){
         }
         for (var prop in args) {
             if (args.hasOwnProperty(prop)) {
-                if(typeof args[prop] === 'function'){
-                    obj[prop] = args[prop];
-                    continue;
-                }
-                obj['get'+capLetter(prop)] = getProperty(prop);
-                obj['set'+capLetter(prop)] = setProperty(prop);
+                obj[prop] = args[prop];
+                pubObj['get'+capLetter(prop)] = getProperty(prop);
+                pubObj['set'+capLetter(prop)] = setProperty(prop);
             }
         }
-        
-        for( var prop in args ){
-            if (args.hasOwnProperty(prop)) {
-                if(typeof args[prop] === 'function'){ continue; }
-                obj['set'+capLetter(prop)](args[prop]);
-            }
-        }
-        return obj;
+
+        return pubObj;
 };
  
 var p = new Person({
    name: 'Jack', 
    age: '10',
-   jump: function(){ return "My name is " + this.name + " and I can jump.";}
 });
 console.log(p.age);
 console.log(p.name);
 console.log(p.getAge());
+console.log(p.getName());
 p.setAge(11);
 p.setName('Joe');
 console.log(p.getAge());
 console.log(p.getName());
-console.log(p.jump()) // “My name is Jack and I can jump.”
